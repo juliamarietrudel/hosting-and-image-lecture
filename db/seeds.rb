47062574db4295401484fde1e3ad83e96c02bc15
Julia Trudel <juliamarietrudel96@gmail.com>
2026-05-29 10:36:50 -0400

    image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 Article.destroy_all
 
 puts "All articles deleted!"
@@ -8,3 +10,9 @@ article_one = Article.new(
 "
 )
 article_one.save!
+
+
+file = URI.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg").open
+article = Article.new(title: "NES", body: "A great console")
+article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.save
